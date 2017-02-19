1a849c68e676509716b33ade3d48e22ae7822868
salody <truman1988@163.com>
2017-02-19 17:42:48 +0800

    实现doctor-details中的slide-tab功能

--- air-life-app/www/js/controllers.js
+++ air-life-app/www/js/controllers.js
@@ -19,8 +19,27 @@ angular.module('starter.controllers', []);
  * Created by xianmengadc on 17-2-17.
  */
 angular.module('starter.controllers')
-  .controller('AirClinicDoctorDetailsCtrl', function ($scope) {
+  .controller('AirClinicDoctorDetailsCtrl', function ($scope,$ionicSlideBoxDelegate) {
+    $scope.slideIndex = 0;
 
+    /*debugger;*/
+    $scope.slideChanged = function(index) {
+      $scope.slideIndex = index;
+      console.log(index);
+
+      if ($scope.slideIndex == 0){
+        console.log("slide 1");
+      }
+
+      else if ($scope.slideIndex == 1){
+        console.log("slide 2");
+      }
+
+    };
+
+    $scope.activeSlide = function (index) {
+      $ionicSlideBoxDelegate.slide(index);
+    };
   });
 
 /**
--- air-life-app/www/js/controllers/air-clinic-doctor-details.js
+++ air-life-app/www/js/controllers/air-clinic-doctor-details.js
@@ -2,6 +2,25 @@
  * Created by xianmengadc on 17-2-17.
  */
 angular.module('starter.controllers')
-  .controller('AirClinicDoctorDetailsCtrl', function ($scope) {
+  .controller('AirClinicDoctorDetailsCtrl', function ($scope,$ionicSlideBoxDelegate) {
+    $scope.slideIndex = 0;
 
+    /*debugger;*/
+    $scope.slideChanged = function(index) {
+      $scope.slideIndex = index;
+      console.log(index);
+
+      if ($scope.slideIndex == 0){
+        console.log("slide 1");
+      }
+
+      else if ($scope.slideIndex == 1){
+        console.log("slide 2");
+      }
+
+    };
+
+    $scope.activeSlide = function (index) {
+      $ionicSlideBoxDelegate.slide(index);
+    };
   });
