5366ca0fb6800dd1e3347d8930377e17c4cd7893
xianmengadc <924986774@qq.com>
2017-02-20 18:22:18 +0800

    增加clinic-home诊所信息网页

--- air-life-app/www/js/controllers.js
+++ air-life-app/www/js/controllers.js
@@ -64,6 +64,11 @@ angular.module('starter.controllers')
 /**
  * Created by xianmengadc on 17-2-17.
  */
+angular.module('starter.controllers')
+  .controller('airClinicHomeCtrl', ['$scope','$state',function ($scope,$state) {
+
+
+  }]);
 
 /**
  * Created by salody on 17-2-20.
--- air-life-app/www/js/controllers/air-clinic-home.js
+++ air-life-app/www/js/controllers/air-clinic-home.js
@@ -1,3 +1,8 @@
 /**
  * Created by xianmengadc on 17-2-17.
  */
+angular.module('starter.controllers')
+  .controller('airClinicHomeCtrl', ['$scope','$state',function ($scope,$state) {
+
+
+  }]);
