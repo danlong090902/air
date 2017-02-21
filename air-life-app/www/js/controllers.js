9877e7e3054c5773b18bce7dd4b47d75e0febffe
salody <truman1988@163.com>
2017-02-21 14:08:33 +0800

    完成支付选择功能，默认选择微信支付

--- air-life-app/www/js/controllers.js
+++ air-life-app/www/js/controllers.js
@@ -241,9 +241,13 @@ angular.module('starter.controllers')
  * Created by salody on 17-2-20.
  */
 angular.module('starter.controllers')
-  .controller('AirClinicPayCtrl', function () {
-
+  .controller('AirClinicPayCtrl', function ($scope) {
+    $scope.button = 'a';
+    $scope.activeButton = function (name) {
+      $scope.button = name;
+    }
   });
+
 
 
 /**
--- air-life-app/www/js/controllers/air-clinic-pay.js
+++ air-life-app/www/js/controllers/air-clinic-pay.js
@@ -2,7 +2,11 @@
  * Created by salody on 17-2-20.
  */
 angular.module('starter.controllers')
-  .controller('AirClinicPayCtrl', function () {
-
+  .controller('AirClinicPayCtrl', function ($scope) {
+    $scope.button = 'a';
+    $scope.activeButton = function (name) {
+      $scope.button = name;
+    }
   });
+
 
