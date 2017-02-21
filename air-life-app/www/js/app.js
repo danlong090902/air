6947d6a54b7a068689693f1b145f598aceb8888e
xianmengadc <924986774@qq.com>
2017-02-21 14:31:52 +0800

    删除app.js内多余代码

--- air-life-app/www/js/app.js
+++ air-life-app/www/js/app.js
@@ -1,12 +1,4 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers'])
-
   .run(function ($ionicPlatform) {
     $ionicPlatform.ready(function () {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -14,7 +6,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
         cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
         cordova.plugins.Keyboard.disableScroll(true);
-
       }
       if (window.StatusBar) {
         // org.apache.cordova.statusbar required
@@ -36,59 +27,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 
     $ionicConfigProvider.platform.ios.views.transition('ios');
     $ionicConfigProvider.platform.android.views.transition('android');
-    // Ionic uses AngularUI Router which uses the concept of states
-    // Learn more here: https://github.com/angular-ui/ui-router
-    // Set up the various states which the app can be in.
-    // Each state's controller can be found in controllers.js
     $stateProvider
-
-    // setup an abstract state for the tabs directive
-    //   .state('tab', {
-    //   url: '/tab',
-    //   abstract: true,
-    //   templateUrl: 'templates/tabs.html'
-    // })
-    //
-    // // Each tab has its own nav history stack:
-    //
-    // .state('tab.dash', {
-    //   url: '/dash',
-    //   views: {
-    //     'tab-dash': {
-    //       templateUrl: 'templates/tab-dash.html',
-    //       controller: 'DashCtrl'
-    //     }
-    //   }
-    // })
-    //
-    // .state('tab.chats', {
-    //     url: '/chats',
-    //     views: {
-    //       'tab-chats': {
-    //         templateUrl: 'templates/tab-chats.html',
-    //         controller: 'ChatsCtrl'
-    //       }
-    //     }
-    //   })
-    //   .state('tab.chat-detail', {
-    //     url: '/chats/:chatId',
-    //     views: {
-    //       'tab-chats': {
-    //         templateUrl: 'templates/chat-detail.html',
-    //         controller: 'ChatDetailCtrl'
-    //       }
-    //     }
-    //   })
-    //
-    // .state('tab.account', {
-    //   url: '/account',
-    //   views: {
-    //     'tab-account': {
-    //       templateUrl: 'templates/tab-account.html',
-    //       controller: 'AccountCtrl'
-    //     }
-    //   }
-    // })
     //  主页tabs
       .state('airTabs', {
         url: '/air-tabs',
@@ -115,18 +54,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
           }
         }
       })
-      // //我的服务
-      // .state('airClinicMyService', {
-      //   url: '/air-clinic-my-service',
-      //   templateUrl: 'templates/air-clinic-my-service.html',
-      //   controller: 'AirClinicMyServiceCtrl'
-      // })
-      //个人中心
-      // .state('airClinicPersonalCenter', {
-      //   url: '/air-clinic-personal-center',
-      //   templateUrl: 'templates/air-clinic-personal-center.html',
-      //   controller: 'AirClinicPersonalCenterCtrl'
-      // })
       //  tabs个人中心
       .state('airTabs.airClinicPersonalCenter', {
         url: '/air-clinic-personal-center',
