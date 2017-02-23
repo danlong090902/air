74124f1ab06d09b4b8c6dc996a8ef23844c71283
xianmengadc <924986774@qq.com>
2017-02-23 17:13:47 +0800

    同上

--- air-life-app/www/js/controllers.js
+++ air-life-app/www/js/controllers.js
@@ -70,13 +70,17 @@ angular.module('starter.controllers')
  */
 angular.module('starter.controllers')
   .controller('AirClinicDepartmentCtrl',['$scope','AirClinicDepartment',function ($scope,AirClinicDepartment) {
-    console.log($scope.userData.token);
+    $scope.clinicDepartment=null;
+    $scope.clinicSpecial=[];
     AirClinicDepartment.login({
       token:$scope.userData.token,
       hospital_id:2
     })
       .then(function (data) {
-        console.log(data);
+        console.log(data.data.result);
+        $scope.clinicDepartment=data.data.result;
+        $scope.clinicSpecial=data.data.result.specialities;
+        console.log($scope.clinicSpecial);
       }, function () {
 
       });
